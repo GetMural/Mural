7ae40979d5ce5d2b92de019a5ceba202b0fd6fc4
Flowdeeps <speedwolf@gmail.com>
2017-03-20 20:27:19 +0000

    Converted all partials to fragments and partials with routes so I can call each of them individually if I need to as I may when I start to build forms programatically

--- app.js
+++ app.js
@@ -35,19 +35,155 @@ app.get('/', function (req, res){
 	});
 });
 
-// Editor View
+// Editor Views
+
+// Main Editor View
 app.get('/editor', function (req, res){
 	res.render('editor/editor', {
 		meta: data["meta"],
 		items: data["items"],
 		partials: {
-			nav: 'partials/nav',
-			formcontrols: 'partials/formcontrols',
-			title: 'partials/title',
-			subtitle: 'partials/subtitle',
-			meta: 'partials/meta',
-			videobackground: 'partials/videobackground',
-			textcentred: 'partials/textcentred'
+			editor: 'editor',
+			editornav: 'fragments/editornav'
+		}
+	});
+});
+
+// Editor Fragments
+
+// Form Controls Fragment
+app.get('/editor/fragment/formcontrols', function(req, res){
+	res.render('editor/fragments/formcontrols', {
+		partials: {
+			formcontrols: 'formcontrols'
+		}
+	});
+});
+
+// Fullpage Toggle Fragment
+app.get('/editor/fragment/fullpage', function(req, res){
+	res.render('editor/fragments/fullpage', {
+		partials: {
+			fullpage: 'fullpage'
+		}
+	});
+});
+
+// Image Fragment
+app.get('/editor/fragment/image', function(req, res){
+	res.render('editor/fragments/image', {
+		partials: {
+			image: 'image'
+		}
+	});
+});
+
+// Image Sources Fragment
+app.get('/editor/fragment/imagesources', function(req, res){
+	res.render('editor/fragments/imagesources', {
+		partials: {
+			imagesources: 'imagesources'
+		}
+	});
+});
+
+// Intro Fragment
+app.get('/editor/fragment/intro', function(req, res){
+	res.render('editor/fragments/intro', {
+		partials: {
+			intro: 'intro'
+		}
+	});
+});
+
+// Slide Fragment
+app.get('/editor/fragment/slide', function(req, res){
+	res.render('editor/fragments/slide', {
+		partials: {
+			slide: 'slide',
+			image: 'image',
+			title: 'title'
+		}
+	});
+});
+
+// Subtitle Fragment
+app.get('/editor/fragment/subtitle', function(req, res){
+	res.render('editor/fragments/subtitle', {
+		partials: {
+			subtitle: 'subtitle'
+		}
+	});
+});
+
+// Text Fragment
+app.get('/editor/fragment/text', function(req, res){
+	res.render('editor/fragments/text', {
+		partials: {
+			text: 'text'
+		}
+	});
+});
+
+// Title Fragment
+app.get('/editor/fragment/title', function(req, res){
+	res.render('editor/fragments/title', {
+		partials: {
+			title: 'title'
+		}
+	});
+});
+
+// Video Sources Fragment
+app.get('/editor/fragment/videosources', function(req, res){
+	res.render('editor/fragments/videosources', {
+		partials: {
+			videosources: 'videosources'
+		}
+	});
+});
+
+// Editor Components (composed fragments)
+
+// Meta Info Page
+app.get('/editor/page/meta', function(req, res){
+	res.render('editor/pages/meta');
+});
+
+// Textcentred Page
+app.get('/editor/page/textcentred', function(req, res){
+	res.render('editor/pages/textcentred');
+});
+
+// Imagebackground Page
+app.get('/editor/page/imagebackground', function(req, res){
+	res.render('editor/pages/imagebackground', {
+		partials: {
+			fullpage: '../fragments/fullpage',
+			imagebackground: '../fragments/imagesources',
+			text: '../fragments/text',
+			title: '../fragments/title',
+			subtitle: '../fragments/subtitle'
+		}
+	});
+});
+
+// Slideshow Horizontal Page
+app.get('/editor/page/slideshowhorizontal', function(req, res){
+	res.render('editor/pages/slideshowhorizontal', {
+		partials: {
+			title: '../fragments/title'
+		}
+	});
+});
+
+// Videobackground Page
+app.get('/editor/page/videobackground', function(req, res){
+	res.render('editor/pages/videobackground', {
+		partials: {
+			formcontrols: '../fragments/formcontrols',
+			image: '../fragments/image',
+			videobackground: 'videobackground'
 		}
 	});
 });
@@ -105,7 +241,7 @@ var server = app.listen(8000, function() {
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.post('/update', function(res, req){
+app.post('/update', function(req, res){
 	console.log(req.body.meta);
 });
 
--- assets/js/editor.js
+++ assets/js/editor.js
@@ -1,20 +1,76 @@
 document.addEventListener("DOMContentLoaded", function (event) {
 
-  var url = 'data/storyboard.json';
+  var loadMaterial = function () {
+    $.material.init();
+  };
 
-  var saveData = function(meta, items) {
+  var url = '/data/storyboard.json';
+
+  var loadPages = function () {
+    var nav = document.getElementsByClassName('js-Nav'),
+        navItems = document.getElementsByClassName('js-NavLink'),
+        bodyElement = document.getElementsByClassName('js-Load');
+
+    for (var i = 0; i < navItems.length; i++) {
+      navItems[i].addEventListener('click', function (event) {
+        event.preventDefault();
+        var navItem = this,
+            link = this.href;
+        $(bodyElement).load(link, function (response, status, xhr) {
+          if ( status !== "error" ) {
+            loadData();
+          }
+        });
+      });
+    };
+  };
+  loadPages();
+
+  var loadData = function (meta, items) {
+
+    var XHR = new XMLHttpRequest();
+
+    XHR.onreadystatechange = function () {
+      if (XHR.readyState == XMLHttpRequest.DONE) {
+        var data = JSON.parse(XHR.responseText),
+            meta = data["meta"],
+            items = data["items"];
+        loadMaterial();
+        populateForms(meta, items);
+      };
+    };
+    XHR.open('GET', url, true);
+    XHR.send(null);
+  };
+
+  var populateForms = function (meta, items) {
+
+    // check for the forms
+    // meta information
+    var metaFormEl = $('.js-Meta')[0];
+    //
+    var textcentredFormEl = $('.js-TextCentred')[0];
+
+    if (typeof metaFormEl != "undefined") {
+
+    }
+
+  };
+
+  var saveData = function (meta, items) {
 
     var save = document.getElementById('save');
-    var data = {"meta":{}};
+    var meta = {"meta":{}};
+    var items = {"items":{}};
 
-    save.addEventListener('click', function (e){
-      e.preventDefault();
-      $('input, textarea').each(function(){
+    save.addEventListener('click', function (event){
+      event.preventDefault();
+      $('input, textarea').each(function (){
         if (this.tagName == "textarea") {
           data["meta"][this.id] = this.innerText;
         } else {
           data["meta"][this.id] = this.value;
-        }
+        };
       });
       $.ajax({
         url: '/update',
@@ -22,8 +78,8 @@ document.addEventListener("DOMContentLoaded", function (event) {
         contentType: 'application/json; charset=UTF-8',
         data: JSON.stringify(data),
         type: 'POST',
-        complete: function() {
-          console.log(data);
+        complete: function () {
+          console.log("posted");
         }
       });
     });
