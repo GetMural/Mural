c7ba1ea9ce6ea14b6d659606f0f676ebee336269
Flowdeeps <pete@republicofpete.net>
2017-08-09 22:07:17 +0100

    Temporary tasks until I replace them with native npm build tools

---
+++ gulpfile.js
@@ -0,0 +1,35 @@
+var gulp        = require('gulp');
+var rename      = require('gulp-rename');
+var sass        = require('gulp-sass');
+var sourcemaps  = require('gulp-sourcemaps');
+var mustache    = require('gulp-mustache');
+
+var data        = require('./data/storyboard.json');
+
+// Gulp Sass Task
+gulp.task('sass', function() {
+  console.log('Building sass files');
+  gulp.src('./views/scss/{,*/}*.{scss,sass}')
+    .pipe(sourcemaps.init())
+    .pipe(sass({
+      errLogToConsole: true
+    }))
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest('./public/css'));
+});
+
+// Gulp Mustache Task
+gulp.task('mustache', function() {
+  console.log('Building templates');
+  gulp.src("./views/*.html")
+      .pipe(mustache(data,{},{}))
+      .pipe(rename('index.html'))
+      .pipe(gulp.dest("./public"));
+});
+
+// Gulp watch tasks
+gulp.task('watch', ['sass', 'html'], function () {
+  gulp.watch('./data/**/*.json', ['mustache']);
+  gulp.watch('./views/scss/**/*.scss', ['sass']);
+  gulp.watch('./views/**/*.html', ['mustache']);
+});
