89f9e2006908b889df0367745d000096c9f55ff6
Mark Lewis <thnkloud9@gmail.com>
2017-12-18 18:50:00 +0100

    feature(editor): partial structure editor

--- public/js/editornav.js
+++ public/js/editornav.js
@@ -8,3 +8,36 @@ document.addEventListener('DOMContentLoaded', function () {
 		$(bodyElement).load(link);
 	});
 });
+
+// Drag and Drop
+var source;
+
+function isbefore(a, b) {
+    if (a.parentNode == b.parentNode) {
+        for (var cur = a; cur; cur = cur.previousSibling) {
+            if (cur === b) {
+                return true;
+            }
+        }
+    }
+    return false;
+}
+
+function dragenter(e) {
+    var targetelem = e.target;
+    if (targetelem.nodeName == "TD") {
+        targetelem = targetelem.parentNode;
+    }
+
+    if (isbefore(source, targetelem)) {
+        targetelem.parentNode.insertBefore(source, targetelem);
+    } else {
+        targetelem.parentNode.insertBefore(source, targetelem.nextSibling);
+    }
+}
+
+function dragstart(e) {
+    source = e.target;
+    e.dataTransfer.effectAllowed = 'move';
+}
+
