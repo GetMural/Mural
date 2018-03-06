a1c69e1e131638e03791af7c47d57e4e4b184db0
Naomi Aro <naomiaro@gmail.com>
2018-03-06 21:58:45 +0200

    fix dynamic content for centredtext

--- public/js/dynamic.js
+++ public/js/dynamic.js
@@ -1,36 +1,34 @@
 var $ = jQuery;
-var itemEditor = $('#item-editor');
+const itemEditor = $('#item-editor');
 
 itemEditor.on('click', '.js-Remove', function (e) {
   e.preventDefault();
-  $(this).parents('.js-Slide').remove();
+  $(this).parents('.js-Dynamic').remove();
   updateIndices();
 });
 
 itemEditor.on('click', '.js-AddSlide', function (e) {
   e.preventDefault();
-  var textEl = document.createElement('div');
-  var textBox = '/editor/fragment/slide';
-  $(textEl).load(textBox, function () {
-    itemEditor.find('.js-ContentDynamic').append($(textEl).html());
-    updateIndices();
-  });
+  addDynamic('slide', 'images');
 });
 
 itemEditor.on('click', '.js-AddImage', function (e) {
   e.preventDefault();
-  var imgEl = document.createElement('div');
-  imgEl.className = 'temp';
-  var imgBox = '/editor/fragment/snippetimage';
-  $(imgEl).load(imgBox, function () {
-    $(imgEl).appendTo('.js-ContentDynamic');
-    instantiateRemove();
-  });
+  addDynamic('snippetimage', 'snippets');
 });
 
-function updateIndices() {
-  itemEditor.find('.js-Slide').each(function (i) {
-    $(this).find("[name*='images']").each(function () {
+function addDynamic(template, name) {
+  const el = document.createElement('div');
+  const fragment = `/editor/fragment/${template}`;
+  $(el).load(fragment, function () {
+    itemEditor.find('.js-ContentDynamic').append($(el).html());
+    updateIndices(name);
+  });
+}
+
+function updateIndices(prefix) {
+  itemEditor.find('.js-Dynamic').each(function (i) {
+    $(this).find(`[name*='${prefix}']`).each(function () {
       const name = $(this).attr('name');
       $(this).attr('name', name.replace(/\[\d?\]/, `[${i}]`));
     });
