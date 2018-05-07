433673c5f6255385b2f985413336d41710b1814f
Naomi Aro <naomiaro@gmail.com>
2018-05-07 23:02:13 +0300

    fixup window

--- public/js/index.js
+++ public/js/index.js
@@ -5,7 +5,7 @@ $(function() {
     });
 
     $('#btn-download').on('click', function() {
-        window.open('/editor/buyusbeer', '', 'width=620,height=480');
+        window.open('/editor/buyusbeer', '', 'width=620,height=700');
     });
 
     // Preview Refresh Event Listener
