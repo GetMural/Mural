b0944507e748e3038c03ae8a8fb9b1e43d1d568f
flowdeeps <speedwolf@gmail.com>
2018-07-12 17:02:39 +0100

    Feat: Text Bounding Box - Active state flag added to videobackground

--- routes/editor.js
+++ routes/editor.js
@@ -659,6 +659,7 @@ router.post('/page/videobackground/id/:id', function (req, res) {
 
         // format and save new values to videobackground
         var fullpage = (newItem['fullpage'] === 'on') ? true : false;
+        var active = (newItem['bg-active'] === 'on') ? true : false;
         item['format'] = { fullpage: fullpage };
         item['backgroundprops'] = {
           value: newItem['bg-colour'],
