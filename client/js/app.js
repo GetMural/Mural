8d230df3f6ba5878490d8eac50040b540455a535
Naomi Aro <naomiaro@gmail.com>
2019-03-21 23:22:55 +0200

    Tuska story parallax updates

--- client/js/app.js
+++ client/js/app.js
@@ -25,19 +25,45 @@ const MoveItItem = function(el){
   this.el = $(el);
   this.container = this.el.parent('.part');
   this.speed = parseInt(this.el.attr('data-scroll-speed'));
-  this.lastTop = null;
+  // 60 fps
+  this.fpsInterval = 1000 / 60;
+  this.top = null;
+  this.inView = false;
+  this.rafID = null;
+  this.then = null;
+};
+
+MoveItItem.prototype.animate = function(time) {
+  // calc elapsed time since last loop
+  const now = time;
+  const elapsed = now - this.then;
+
+  // if enough time has elapsed, draw the next frame
+  if (elapsed > this.fpsInterval) {
+    // Get ready for next frame by setting then=now, but...
+    // Also, adjust for fpsInterval not being multiple of 16.67
+    this.then = now - (elapsed % this.fpsInterval);
+    this.el.css('transform', 'translateY(' + -(this.top / this.speed) + 'px)');
+  }
+  this.rafID = requestAnimationFrame(this.animate.bind(this));
 };
 
 MoveItItem.prototype.update = function(scrollTop){
   if (this.container.hasClass('inviewport')) {
-    this.el.css('willChange', 'transform');
-    const top = scrollTop - this.container.offset().top;
-    if (this.lastTop !== top) {
-      this.el.css('transform', 'translateY(' + -(top / this.speed) + 'px)');
-      this.lastTop = top;
-    } 
+    if (!this.inView) {
+      this.el.css('willChange', 'transform');
+      this.then = performance.now();
+      this.rafID = requestAnimationFrame(this.animate.bind(this));
+    }
+    
+    this.top = scrollTop - this.container.offset().top;
+    this.inView = true;
   } else {
-    this.el.css('willChange', 'auto');
+    if (this.inView) {
+      cancelAnimationFrame(this.rafID);
+      this.el.css('willChange', 'auto');
+    } 
+    this.inView = false;
   }
 };
 
--- public/app.e6195b35804c11fde9cb.js
+++ public/app.e6195b35804c11fde9cb.js
@@ -10583,21 +10583,47 @@ $.fn.moveIt = function () {
 var MoveItItem = function MoveItItem(el) {
   this.el = $(el);
   this.container = this.el.parent('.part');
-  this.speed = parseInt(this.el.attr('data-scroll-speed'));
-  this.lastTop = null;
+  this.speed = parseInt(this.el.attr('data-scroll-speed')); // 60 fps
+
+  this.fpsInterval = 1000 / 60;
+  this.top = null;
+  this.inView = false;
+  this.rafID = null;
+  this.then = null;
+};
+
+MoveItItem.prototype.animate = function (time) {
+  // calc elapsed time since last loop
+  var now = time;
+  var elapsed = now - this.then; // if enough time has elapsed, draw the next frame
+
+  if (elapsed > this.fpsInterval) {
+    // Get ready for next frame by setting then=now, but...
+    // Also, adjust for fpsInterval not being multiple of 16.67
+    this.then = now - elapsed % this.fpsInterval;
+    this.el.css('transform', 'translateY(' + -(this.top / this.speed) + 'px)');
+  }
+
+  this.rafID = requestAnimationFrame(this.animate.bind(this));
 };
 
 MoveItItem.prototype.update = function (scrollTop) {
   if (this.container.hasClass('inviewport')) {
-    this.el.css('willChange', 'transform');
-    var top = scrollTop - this.container.offset().top;
-
-    if (this.lastTop !== top) {
-      this.el.css('transform', 'translateY(' + -(top / this.speed) + 'px)');
-      this.lastTop = top;
+    if (!this.inView) {
+      this.el.css('willChange', 'transform');
+      this.then = performance.now();
+      this.rafID = requestAnimationFrame(this.animate.bind(this));
     }
+
+    this.top = scrollTop - this.container.offset().top;
+    this.inView = true;
   } else {
-    this.el.css('willChange', 'auto');
+    if (this.inView) {
+      cancelAnimationFrame(this.rafID);
+      this.el.css('willChange', 'auto');
+    }
+
+    this.inView = false;
   }
 };
 
