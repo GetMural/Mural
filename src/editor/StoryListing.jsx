6a0d209ac8dacfd389ef97989314888cad81483f
Naomi Aro <naomi.aro@zalando.fi>
2019-10-08 22:58:02 +0300

    basic item adding

--- src/editor/StoryListing.jsx
+++ src/editor/StoryListing.jsx
@@ -1,33 +1,50 @@
-import React from 'react';
-import { H3 } from '@bootstrap-styled/v4';
+import React, { Component } from 'react';
+import { H3, Select, Option } from '@bootstrap-styled/v4';
 import { WorkspaceConsumer } from '../WorkspaceContext';
 import Layout from './Layout';
 import { StoryItem } from '../models/StoryModel';
 import ItemList from './ItemList';
 
-const StoryListing = props => {
-  return (
-    <WorkspaceConsumer>
-      {({ storyState, currentStory }) => {
-        return (
-          <Layout>
-            <H3>Current Story: {currentStory}</H3>
-            <div>
-              <label htmlFor="new_story">New Item</label>
-              <button
-                onClick={() => {
-                  storyState.addItem(StoryItem.create());
-                }}
-              >
-                ImageBackground
-              </button>
-            </div>
-            <ItemList items={storyState.items} />
-          </Layout>
-        );
-      }}
-    </WorkspaceConsumer>
-  );
-};
+class StoryListing extends Component {
+  state = {
+    itemType: 'ImageBackground',
+  };
+  render() {
+    return (
+      <WorkspaceConsumer>
+        {({ storyState, currentStory }) => {
+          return (
+            <Layout>
+              <H3>Current Story: {currentStory}</H3>
+              <div>
+                <label htmlFor="new_story">New Item</label>
+                <Select
+                  onChange={e => {
+                    this.setState({ itemType: e.target.value });
+                  }}
+                >
+                  <Option value="ImageBackground">
+                    ImageBackground
+                  </Option>
+                  <Option value="ImageParallax">ImageParallax</Option>
+                </Select>
+                <button
+                  onClick={() => {
+                    storyState.addItem(
+                      StoryItem.create({ type: this.state.itemType }),
+                    );
+                  }}
+                >
+                  Add
+                </button>
+              </div>
+              <ItemList items={storyState.items} />
+            </Layout>
+          );
+        }}
+      </WorkspaceConsumer>
+    );
+  }
+}
 
 export default StoryListing;
