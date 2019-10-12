78fc7903753ce9e4b5d365911d8b2cb5a46778f8
Naomi Aro <naomi.aro@zalando.fi>
2019-10-13 01:37:27 +0300

    try another entry

--- .webpack.config.js
+++ .webpack.config.js
@@ -1,11 +1,15 @@
 const webpack = require('webpack');
-const {appendWebpackPlugin} = require('@rescripts/utilities');
+const { appendWebpackPlugin } = require('@rescripts/utilities');
 // define child rescript
 module.exports = config => {
+  config.entry = {
+    app: ['./src/index.js'],
+    mural: ['./src/client/app.js'],
+  };
   config.target = 'electron-renderer';
   const jQueryPlugin = new webpack.ProvidePlugin({
-    $: "jquery",
-    jQuery: "jquery"
+    $: 'jquery',
+    jQuery: 'jquery',
   });
   return appendWebpackPlugin(jQueryPlugin, config);
-}
+};
