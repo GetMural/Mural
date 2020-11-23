149145af30c9730fa3325316c3547e19456dc46e
naomiaro <naomiaro@gmail.com>
2020-11-23 15:12:22 +0000

    Revert "try just zips"
    This reverts commit 42dfd5b76855ef9d335067f8cfafb4ac981c3ae2.

--- main.js
+++ main.js
@@ -1,5 +1,10 @@
 const { app, BrowserWindow, Tray, Menu, dialog, nativeImage } = require("electron");
 
+// Handle creating/removing shortcuts on Windows when installing/uninstalling.
+if (require('electron-squirrel-startup')) { // eslint-disable-line global-require
+  app.quit();
+}
+
 const path = require("path");
 const fs = require("fs-extra");
 
