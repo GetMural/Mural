444d40875c40795ff17922533ee4b4660ca47c2d
Flowdeeps <speedwolf@gmail.com>
2021-06-06 15:55:17 +0200

    Passing props to a child component to render different styles as we'll need to do once it's actually being used

--- src/components/ContentBlockSelector/ContentBlockSelector.js
+++ src/components/ContentBlockSelector/ContentBlockSelector.js
@@ -1,12 +1,23 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+import './ContentBlockSelector.scss';
 
 export class ContentBlockSelector extends Component {
   render() {
     return (
       <>
       {console.log(this.props)}
-      <div>ContentBlockSelector</div>
+      <div className='ContentBlockSelector'>
+          <div className={ this.props.type }>
+          ContentBlockSelector
+        </div>
+      </div>
       </>
     );
   }
+}
+
+ContentBlockSelector.propTypes = {
+  type: PropTypes.string.isRequired
 }
