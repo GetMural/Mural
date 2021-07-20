e70f7c0e619a96919c6381e78f95b177eed4ab48
Edouard Richard <vied12@gmail.com>
2021-07-20 15:48:43 +0200

    feat: electron app

---
+++ electron/preload.js
@@ -0,0 +1,5 @@
+const { contextBridge, ipcRenderer } = require('electron')
+
+contextBridge.exposeInMainWorld('electron', {
+  doThing: () => ipcRenderer.send('do-a-thing'),
+})
---
+++ electron/start.ts
@@ -0,0 +1,45 @@
+const electron = require('electron')
+const app = electron.app
+const BrowserWindow = electron.BrowserWindow
+
+const path = require('path')
+const isDev = require('electron-is-dev')
+
+let mainWindow
+function createWindow() {
+  mainWindow = new BrowserWindow({
+    width: 900,
+    height: 680,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js'),
+      // nodeIntegration: true,
+      // contextIsolation: false,
+      // enableRemoteModule: true,
+    },
+  })
+  mainWindow.loadURL(
+    isDev
+      ? 'http://localhost:3000'
+      : `file://${path.join(__dirname, '../build/index.html')}`
+  )
+  if (isDev) {
+    // Open the DevTools.
+    //BrowserWindow.addDevToolsExtension('<location to your react chrome extension>');
+    mainWindow.webContents.openDevTools()
+  }
+  mainWindow.on('closed', () => (mainWindow = null))
+}
+
+app.on('ready', createWindow)
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+app.on('activate', () => {
+  if (mainWindow === null) {
+    createWindow()
+  }
+})
