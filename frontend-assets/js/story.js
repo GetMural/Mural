3e0003918e12f07abfa0b00b351cf27eb6e85cbb
Loïc Nogues <nogues.loic@gmail.com>
2021-11-29 17:32:04 +0100

    start video before focus

--- frontend-assets/js/story.js
+++ frontend-assets/js/story.js
@@ -14,6 +14,72 @@ const youtubeMedia = require('./media/youtube')
 const vimeoMedia = require('./media/vimeo')
 const dailymotionMedia = require('./media/dailymotion')
 
+$.fn.moveIt = function () {
+  var $window = $(window)
+  var instances = []
+
+  $(this).each(function () {
+    instances.push(new MoveItItem($(this)))
+  })
+
+  window.addEventListener(
+    'scroll',
+    function (e) {
+      const scrollTop = $window.scrollTop()
+      instances.forEach(function (inst) {
+        inst.update(scrollTop)
+      })
+    },
+    { passive: true }
+  ) // TODO check compatibility
+}
+
+const MoveItItem = function (el) {
+  this.el = $(el)
+  this.container = this.el.parent('.part')
+  this.speed = parseInt(this.el.attr('data-scroll-speed'))
+  // 60 fps
+  this.fpsInterval = 1000 / 60
+  this.top = null
+  this.inView = false
+  this.rafID = null
+  this.then = null
+}
+
+MoveItItem.prototype.animate = function (time) {
+  // calc elapsed time since last loop
+  const now = time
+  const elapsed = now - this.then
+
+  // if enough time has elapsed, draw the next frame
+  if (elapsed > this.fpsInterval) {
+    // Get ready for next frame by setting then=now, but...
+    // Also, adjust for fpsInterval not being multiple of 16.67
+    this.then = now - (elapsed % this.fpsInterval)
+    this.el.css('transform', 'translateY(' + -(this.top / this.speed) + 'px)')
+  }
+  this.rafID = requestAnimationFrame(this.animate.bind(this))
+}
+
+MoveItItem.prototype.update = function (scrollTop) {
+  if (this.container.hasClass('inviewport')) {
+    if (!this.inView) {
+      this.el.css('willChange', 'transform')
+      this.then = performance.now()
+      this.rafID = requestAnimationFrame(this.animate.bind(this))
+    }
+
+    this.top = scrollTop - this.container.offset().top
+    this.inView = true
+  } else {
+    if (this.inView) {
+      cancelAnimationFrame(this.rafID)
+      this.el.css('willChange', 'auto')
+    }
+    this.inView = false
+  }
+}
+
 // Override this function so we can change the arrow keys.
 blueimp.prototype.onkeydown = function (event) {
   if (this.options.storyItem.active) {
@@ -93,30 +159,30 @@ function init() {
 
   $story.on('itemfocus', function (ev, item) {
     if (item.data.video) {
-      videoMedia.playBackgroundVideo(item.index, getVideoAttrs(item))
+      // videoMedia.playBackgroundVideo(item.index, getVideoAttrs(item))
       videoMedia.fixBackgroundVideo(item.el)
     }
 
     if (item.data.youtubeId) {
-      youtubeMedia.play(item, isSoundEnabled)
+      // youtubeMedia.play(item, isSoundEnabled)
       youtubeMedia.stick(item)
     }
 
     if (item.data.vimeoVideoId) {
-      vimeoMedia.play(item, isSoundEnabled)
+      // vimeoMedia.play(item, isSoundEnabled)
       vimeoMedia.stick(item)
     }
 
     if (item.data.dailymotionId) {
-      dailymotionMedia.play(item, isSoundEnabled)
+      // dailymotionMedia.play(item, isSoundEnabled)
       dailymotionMedia.stick(item)
     }
 
-    if (item.data.audio) {
-      audioMedia.playBackgroundAudio(item, {
-        muted: !isSoundEnabled,
-      })
-    }
+    // if (item.data.audio) {
+    //   audioMedia.playBackgroundAudio(item, {
+    //     muted: !isSoundEnabled,
+    //   })
+    // }
   })
 
   $story.on('itemblur', function (ev, item) {
@@ -152,6 +218,32 @@ function init() {
     // load another in advance
     if (item.index + 2 < storyItems.length) {
       loadItem(storyItems[item.index + 2])
+    }
+
+    if (item.data.video) {
+      videoMedia.playBackgroundVideo(item.index, getVideoAttrs(item))
+      // videoMedia.fixBackgroundVideo(item.el)
+    }
+
+    if (item.data.youtubeId) {
+      youtubeMedia.play(item, isSoundEnabled)
+      // youtubeMedia.stick(item)
+    }
+
+    if (item.data.vimeoVideoId) {
+      vimeoMedia.play(item, isSoundEnabled)
+      // vimeoMedia.stick(item)
+    }
+
+    if (item.data.dailymotionId) {
+      dailymotionMedia.play(item, isSoundEnabled)
+      // dailymotionMedia.stick(item)
+    }
+
+    if (item.data.audio) {
+      audioMedia.playBackgroundAudio(item, {
+        muted: !isSoundEnabled,
+      })
     }
   })
 
