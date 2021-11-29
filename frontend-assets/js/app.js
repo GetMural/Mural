04ec0a8ee03d592419d285525c2f64f54304130c
Loïc Nogues <nogues.loic@gmail.com>
2021-11-29 14:27:55 +0100

    Split files

--- frontend-assets/js/app.js
+++ frontend-assets/js/app.js
@@ -139,17 +139,17 @@ if (WINDOW_WIDTH >= 1024) {
   attrKey = 'src-phone'
 }
 
-const $story = $('#scrollytelling')
-const scrollStory = $story
-  .scrollStory({
-    contentSelector: '.part',
-    triggerOffset: 0,
-  })
-  .data('plugin_scrollStory')
+// const $story = $('#scrollytelling')
+// const scrollStory = $story
+//   .scrollStory({
+//     contentSelector: '.part',
+//     triggerOffset: 0,
+//   })
+//   .data('plugin_scrollStory')
 
-prepMediaElements(scrollStory)
+// prepMediaElements(scrollStory)
 
-const storyItems = scrollStory.getItems()
+// const storyItems = scrollStory.getItems()
 
 const LOAD_PROMISES = []
 const LOADED_STORY_SECTIONS = []
@@ -167,288 +167,289 @@ function getVideoAttrs(item) {
   }
 }
 
-function loadItem(item) {
-  if (LOADED_STORY_SECTIONS[item.index] !== undefined) {
-    return
-  } else {
-    LOADED_STORY_SECTIONS[item.index] = {
-      loaded: true,
-    }
-  }
+// function loadItem(item) {
+//   console.log('load item', item)
+//   if (LOADED_STORY_SECTIONS[item.index] !== undefined) {
+//     return
+//   } else {
+//     LOADED_STORY_SECTIONS[item.index] = {
+//       loaded: true,
+//     }
+//   }
 
-  const returnPromises = []
+//   const returnPromises = []
 
-  if (item.data.youtubeId) {
-    const youtubeLoaded = youtubeMedia.prepare(scrollStory, item)
-    returnPromises.push(youtubeLoaded)
-  }
+//   if (item.data.youtubeId) {
+//     const youtubeLoaded = youtubeMedia.prepare(scrollStory, item)
+//     returnPromises.push(youtubeLoaded)
+//   }
 
-  if (item.data.vimeoVideoId) {
-    const vimeoLoaded = vimeoMedia.prepare(scrollStory, item)
-    returnPromises.push(vimeoLoaded)
-  }
+//   if (item.data.vimeoVideoId) {
+//     const vimeoLoaded = vimeoMedia.prepare(scrollStory, item)
+//     returnPromises.push(vimeoLoaded)
+//   }
 
-  if (item.data.dailymotionId) {
-    const dailymotionLoaded = dailymotionMedia.prepare(scrollStory, item)
-    returnPromises.push(dailymotionLoaded)
-  }
+//   if (item.data.dailymotionId) {
+//     const dailymotionLoaded = dailymotionMedia.prepare(scrollStory, item)
+//     returnPromises.push(dailymotionLoaded)
+//   }
 
-  if (item.data.image) {
-    const imageLoaded = imageMedia
-      .insertBackgroundImage(item.el, item.data[scrKey])
-      .then((shouldUpdateOffsets) => {
-        if (shouldUpdateOffsets) {
-          scrollStory.updateOffsets()
-        }
-      })
-    returnPromises.push(imageLoaded)
-  }
+//   if (item.data.image) {
+//     const imageLoaded = imageMedia
+//       .insertBackgroundImage(item.el, item.data[scrKey])
+//       .then((shouldUpdateOffsets) => {
+//         if (shouldUpdateOffsets) {
+//           scrollStory.updateOffsets()
+//         }
+//       })
+//     returnPromises.push(imageLoaded)
+//   }
 
-  if (item.data.slideshow) {
-    const slides = item.el.find('.slide-container a').get()
-    const slidePromises = []
+//   if (item.data.slideshow) {
+//     const slides = item.el.find('.slide-container a').get()
+//     const slidePromises = []
 
-    for (let i = 0; i < slides.length; i++) {
-      const a = slides[i]
-      const src = $(a).data(scrKey)
-      const loadPromise = imageMedia.imageLoadPromise(src)
-      slidePromises.push(loadPromise)
-    }
+//     for (let i = 0; i < slides.length; i++) {
+//       const a = slides[i]
+//       const src = $(a).data(scrKey)
+//       const loadPromise = imageMedia.imageLoadPromise(src)
+//       slidePromises.push(loadPromise)
+//     }
 
-    const horizontalSlidePromise = Promise.all(slidePromises).then(() => {
-      blueimp(slides, {
-        container: item.el.find('.blueimp-gallery')[0],
-        urlProperty: attrKey,
-        carousel: true,
-        startSlideshow: false,
-        thumbnailIndicators: true,
-        enableKeyboardNavigation: true,
-        toggleControlsOnEnter: false,
-        storyItem: item, // use this in our custom keydown mod.
-        onslide: function (index, slide) {
-          const info = [
-            { selector: '.slide-caption', attr: 'title' },
-            { selector: '.credits', attr: 'data-credits' },
-          ]
+//     const horizontalSlidePromise = Promise.all(slidePromises).then(() => {
+//       blueimp(slides, {
+//         container: item.el.find('.blueimp-gallery')[0],
+//         urlProperty: attrKey,
+//         carousel: true,
+//         startSlideshow: false,
+//         thumbnailIndicators: true,
+//         enableKeyboardNavigation: true,
+//         toggleControlsOnEnter: false,
+//         storyItem: item, // use this in our custom keydown mod.
+//         onslide: function (index, slide) {
+//           const info = [
+//             { selector: '.slide-caption', attr: 'title' },
+//             { selector: '.credits', attr: 'data-credits' },
+//           ]
 
-          info.forEach(({ selector, attr }) => {
-            const text = this.list[index].getAttribute(attr)
-            const $node = $(this.container).parent().find(selector)
-            $node.empty()
-            if (text) {
-              $node[0].innerHTML = text
-            }
-          })
-        },
-      })
-    })
+//           info.forEach(({ selector, attr }) => {
+//             const text = this.list[index].getAttribute(attr)
+//             const $node = $(this.container).parent().find(selector)
+//             $node.empty()
+//             if (text) {
+//               $node[0].innerHTML = text
+//             }
+//           })
+//         },
+//       })
+//     })
 
-    returnPromises.push(horizontalSlidePromise)
-  }
+//     returnPromises.push(horizontalSlidePromise)
+//   }
 
-  if (item.data.slides) {
-    item.el
-      .find('.bg-image')
-      .each(function (i) {
-        const $el = $(this)
-        const src = $el.data(scrKey)
+//   if (item.data.slides) {
+//     item.el
+//       .find('.bg-image')
+//       .each(function (i) {
+//         const $el = $(this)
+//         const src = $el.data(scrKey)
 
-        const loadPromise = imageMedia.imageLoadPromise(src).then(() => {
-          $el.css('background-image', `url(${src})`)
-        })
+//         const loadPromise = imageMedia.imageLoadPromise(src).then(() => {
+//           $el.css('background-image', `url(${src})`)
+//         })
 
-        returnPromises.push(loadPromise)
-      })
-      .stickybits()
-  }
+//         returnPromises.push(loadPromise)
+//       })
+//       .stickybits()
+//   }
 
-  if (item.data.parallax) {
-    const src = item.data[scrKey]
-    const loadPromise = imageMedia.imageLoadPromise(src).then(() => {
-      item.el.find('.bg-image').css('background-image', `url(${src})`)
-    })
+//   if (item.data.parallax) {
+//     const src = item.data[scrKey]
+//     const loadPromise = imageMedia.imageLoadPromise(src).then(() => {
+//       item.el.find('.bg-image').css('background-image', `url(${src})`)
+//     })
 
-    returnPromises.push(loadPromise)
-  }
+//     returnPromises.push(loadPromise)
+//   }
 
-  if (item.data.video) {
-    const videoLoaded = videoMedia.prepareVideo(
-      scrollStory,
-      item.el,
-      item.index,
-      [
-        {
-          type: 'video/mp4',
-          src: item.data.mp4,
-        },
-        {
-          type: 'video/webm',
-          src: item.data.webm,
-        },
-        {
-          type: 'application/vnd.apple.mpegurl',
-          src: item.data.hls,
-        },
-      ],
-      getVideoAttrs(item)
-    )
+//   if (item.data.video) {
+//     const videoLoaded = videoMedia.prepareVideo(
+//       scrollStory,
+//       item.el,
+//       item.index,
+//       [
+//         {
+//           type: 'video/mp4',
+//           src: item.data.mp4,
+//         },
+//         {
+//           type: 'video/webm',
+//           src: item.data.webm,
+//         },
+//         {
+//           type: 'application/vnd.apple.mpegurl',
+//           src: item.data.hls,
+//         },
+//       ],
+//       getVideoAttrs(item)
+//     )
 
-    returnPromises.push(videoLoaded)
-  }
+//     returnPromises.push(videoLoaded)
+//   }
 
-  if (item.data.audio) {
-    const audioLoaded = audioMedia.prepareAudio(
-      scrollStory,
-      item.el,
-      item.index,
-      [
-        {
-          type: 'audio/mp3',
-          src: item.data.mp3,
-        },
-        {
-          type: 'audio/ogg',
-          src: item.data.ogg,
-        },
-      ],
-      { loop: item.data.loop }
-    )
+//   if (item.data.audio) {
+//     const audioLoaded = audioMedia.prepareAudio(
+//       scrollStory,
+//       item.el,
+//       item.index,
+//       [
+//         {
+//           type: 'audio/mp3',
+//           src: item.data.mp3,
+//         },
+//         {
+//           type: 'audio/ogg',
+//           src: item.data.ogg,
+//         },
+//       ],
+//       { loop: item.data.loop }
+//     )
 
-    returnPromises.push(audioLoaded)
-  }
+//     returnPromises.push(audioLoaded)
+//   }
 
-  return Promise.all(returnPromises)
-}
+//   return Promise.all(returnPromises)
+// }
 
-$story.on('itemfocus', function (ev, item) {
-  if (item.data.video) {
-    videoMedia.playBackgroundVideo(item.index, getVideoAttrs(item))
-    videoMedia.fixBackgroundVideo(item.el)
-  }
+// $story.on('itemfocus', function (ev, item) {
+//   if (item.data.video) {
+//     videoMedia.playBackgroundVideo(item.index, getVideoAttrs(item))
+//     videoMedia.fixBackgroundVideo(item.el)
+//   }
 
-  if (item.data.youtubeId) {
-    youtubeMedia.play(item, isSoundEnabled)
-    youtubeMedia.stick(item)
-  }
+//   if (item.data.youtubeId) {
+//     youtubeMedia.play(item, isSoundEnabled)
+//     youtubeMedia.stick(item)
+//   }
 
-  if (item.data.vimeoVideoId) {
-    vimeoMedia.play(item, isSoundEnabled)
-    vimeoMedia.stick(item)
-  }
+//   if (item.data.vimeoVideoId) {
+//     vimeoMedia.play(item, isSoundEnabled)
+//     vimeoMedia.stick(item)
+//   }
 
-  if (item.data.dailymotionId) {
-    dailymotionMedia.play(item, isSoundEnabled)
-    dailymotionMedia.stick(item)
-  }
+//   if (item.data.dailymotionId) {
+//     dailymotionMedia.play(item, isSoundEnabled)
+//     dailymotionMedia.stick(item)
+//   }
 
-  if (item.data.audio) {
-    audioMedia.playBackgroundAudio(item, {
-      muted: !isSoundEnabled,
-    })
-  }
-})
+//   if (item.data.audio) {
+//     audioMedia.playBackgroundAudio(item, {
+//       muted: !isSoundEnabled,
+//     })
+//   }
+// })
 
-$story.on('itemblur', function (ev, item) {
-  if (item.data.youtubeId) {
-    youtubeMedia.remove(item)
-  }
+// $story.on('itemblur', function (ev, item) {
+//   if (item.data.youtubeId) {
+//     youtubeMedia.remove(item)
+//   }
 
-  if (item.data.vimeoVideoId) {
-    vimeoMedia.remove(item)
-  }
+//   if (item.data.vimeoVideoId) {
+//     vimeoMedia.remove(item)
+//   }
 
-  if (item.data.dailymotionId) {
-    dailymotionMedia.remove(item)
-  }
+//   if (item.data.dailymotionId) {
+//     dailymotionMedia.remove(item)
+//   }
 
-  if (item.data.video) {
-    videoMedia.removeBackgroundVideo(item.el, item.index)
-  }
+//   if (item.data.video) {
+//     videoMedia.removeBackgroundVideo(item.el, item.index)
+//   }
 
-  if (item.data.audio) {
-    audioMedia.removeBackgroundAudio(item.index)
-  }
-})
+//   if (item.data.audio) {
+//     audioMedia.removeBackgroundAudio(item.index)
+//   }
+// })
 
-$story.on('itementerviewport', function (ev, item) {
-  loadItem(item)
+// $story.on('itementerviewport', function (ev, item) {
+//   loadItem(item)
 
-  // load another in advance
-  if (item.index + 1 < storyItems.length) {
-    loadItem(storyItems[item.index + 1])
-  }
+//   // load another in advance
+//   if (item.index + 1 < storyItems.length) {
+//     loadItem(storyItems[item.index + 1])
+//   }
 
-  // load another in advance
-  if (item.index + 2 < storyItems.length) {
-    loadItem(storyItems[item.index + 2])
-  }
-})
+//   // load another in advance
+//   if (item.index + 2 < storyItems.length) {
+//     loadItem(storyItems[item.index + 2])
+//   }
+// })
 
 // parallax.
-$('[data-scroll-speed]').moveIt()
+// $('[data-scroll-speed]').moveIt()
 
-$('.mute').click(function () {
-  const $this = $(this)
-  if ($this.hasClass('muted')) {
-    isSoundEnabled = true
-    $this.removeClass('muted')
-  } else {
-    isSoundEnabled = false
-    $this.addClass('muted')
-  }
+// $('.mute').click(function () {
+//   const $this = $(this)
+//   if ($this.hasClass('muted')) {
+//     isSoundEnabled = true
+//     $this.removeClass('muted')
+//   } else {
+//     isSoundEnabled = false
+//     $this.addClass('muted')
+//   }
 
-  storyItems.forEach(function (item) {
-    if (item.data.video) {
-      const muted = !isSoundEnabled || item.data.muted
-      videoMedia.setMuted(item.index, muted)
-    }
+//   storyItems.forEach(function (item) {
+//     if (item.data.video) {
+//       const muted = !isSoundEnabled || item.data.muted
+//       videoMedia.setMuted(item.index, muted)
+//     }
 
-    if (item.data.audio) {
-      const muted = !isSoundEnabled
-      audioMedia.setMuted(item.index, muted)
-    }
-  })
+//     if (item.data.audio) {
+//       const muted = !isSoundEnabled
+//       audioMedia.setMuted(item.index, muted)
+//     }
+//   })
 
-  youtubeMedia.setMuted(!isSoundEnabled)
-  vimeoMedia.setMuted(!isSoundEnabled)
-  dailymotionMedia.setMuted(!isSoundEnabled)
-})
+//   youtubeMedia.setMuted(!isSoundEnabled)
+//   vimeoMedia.setMuted(!isSoundEnabled)
+//   dailymotionMedia.setMuted(!isSoundEnabled)
+// })
 
-$('.sticks_wrapper').click(function () {
-  $('body').toggleClass('paneOpen')
-})
+// $('.sticks_wrapper').click(function () {
+//   $('body').toggleClass('paneOpen')
+// })
 
-$('nav').on('click', 'li', function () {
-  scrollStory.index(parseInt(this.dataset.id, 10))
-})
+// $('nav').on('click', 'li', function () {
+//   scrollStory.index(parseInt(this.dataset.id, 10))
+// })
 
-const active = scrollStory.getActiveItem()
+// const active = scrollStory.getActiveItem()
 
-scrollStory.getItemsInViewport().forEach(function (item) {
-  const loadPromise = loadItem(item)
+// scrollStory.getItemsInViewport().forEach(function (item) {
+//   const loadPromise = loadItem(item)
 
-  if (loadPromise) {
-    LOAD_PROMISES.push(loadPromise)
-  }
-})
+//   if (loadPromise) {
+//     LOAD_PROMISES.push(loadPromise)
+//   }
+// })
 
-// push two in advance
-if (active.index + 1 < storyItems.length) {
-  const loadPromise = loadItem(storyItems[active.index + 1])
+// // push two in advance
+// if (active.index + 1 < storyItems.length) {
+//   const loadPromise = loadItem(storyItems[active.index + 1])
 
-  if (loadPromise) {
-    LOAD_PROMISES.push(loadPromise)
-  }
-}
+//   if (loadPromise) {
+//     LOAD_PROMISES.push(loadPromise)
+//   }
+// }
 
-// push two in advance
-if (active.index + 2 < storyItems.length) {
-  const loadPromise = loadItem(storyItems[active.index + 2])
+// // push two in advance
+// if (active.index + 2 < storyItems.length) {
+//   const loadPromise = loadItem(storyItems[active.index + 2])
 
-  if (loadPromise) {
-    LOAD_PROMISES.push(loadPromise)
-  }
-}
+//   if (loadPromise) {
+//     LOAD_PROMISES.push(loadPromise)
+//   }
+// }
 
 const form = $('#bypass')
 
@@ -487,44 +488,38 @@ form.find('button').on('click', function (event) {
   }
 })
 
-Promise.all(LOAD_PROMISES)
-  .then(() => {
-    let overlay = document.getElementById('loading_overlay')
-    let playStart = document.getElementById('play_start')
-    playStart.style.display = 'block'
+let overlay = document.getElementById('loading_overlay')
+let playStart = document.getElementById('play_start')
+playStart.style.display = 'block'
 
-    playStart.addEventListener('click', () => {
-      const MURAL_MEDIA = scrollStory.MURAL_AUDIO.concat(
-        scrollStory.MURAL_VIDEO
-      )
-      // load a media element within scope of the user gesture to make sure Safari works.
-      if (MURAL_MEDIA.length) {
-        MURAL_MEDIA[MURAL_MEDIA.length - 1].load()
-      }
+playStart.addEventListener('click', () => {
+  // const MURAL_MEDIA = scrollStory.MURAL_AUDIO.concat(
+  //   scrollStory.MURAL_VIDEO
+  // )
+  // // load a media element within scope of the user gesture to make sure Safari works.
+  // if (MURAL_MEDIA.length) {
+  //   MURAL_MEDIA[MURAL_MEDIA.length - 1].load()
+  // }
 
-      document.body.removeChild(overlay)
-      document.body.classList.remove('frozen')
+  document.body.removeChild(overlay)
+  document.body.classList.remove('frozen')
 
-      if (active.data.video) {
-        videoMedia.playBackgroundVideo(active.index, getVideoAttrs(active))
-        videoMedia.fixBackgroundVideo(active.el)
-      }
+  // if (active.data.video) {
+  //   videoMedia.playBackgroundVideo(active.index, getVideoAttrs(active))
+  //   videoMedia.fixBackgroundVideo(active.el)
+  // }
 
-      if (active.data.audio) {
-        audioMedia.playBackgroundAudio(active, {
-          muted: !isSoundEnabled,
-        })
-      }
+  // if (active.data.audio) {
+  //   audioMedia.playBackgroundAudio(active, {
+  //     muted: !isSoundEnabled,
+  //   })
+  // }
 
-      if (active.data.youtubeId) {
-        youtubeMedia.play(active, isSoundEnabled)
-        youtubeMedia.stick(active)
-      }
+  // if (active.data.youtubeId) {
+  //   youtubeMedia.play(active, isSoundEnabled)
+  //   youtubeMedia.stick(active)
+  // }
 
-      overlay = null
-      playStart = null
-    })
-  })
-  .catch((e) => {
-    console.error(e)
-  })
+  overlay = null
+  playStart = null
+})
