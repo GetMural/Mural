5ca878a480166164e2c0ddef2c30cb86d5fb2909
Naomi Aro <naomiaro@gmail.com>
2021-12-17 21:36:10 -0800

    remove sourcemap

--- frontend-webpack.config.js
+++ frontend-webpack.config.js
@@ -4,7 +4,6 @@ module.exports = {
   entry: {
     app: path.resolve(__dirname, 'frontend-assets', 'js', 'app.js'),
   },
-  devtool: 'source-map',
   output: {
     path: path.resolve(__dirname, 'frontend-assets-build'),
     filename: 'bundle.js',
