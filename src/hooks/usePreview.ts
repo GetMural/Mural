cb013b7550b146026a049433acde67c33c89a1df
Flowdeeps <speedwolf@gmail.com>
2021-12-20 22:08:41 +0100

    MP3 filepaths were only correct for backgroundImage and not for imageAudio elements types.

--- src/hooks/usePreview.ts
+++ src/hooks/usePreview.ts
@@ -54,7 +54,7 @@ export default function usePreview() {
                   audio: item.audio
                     ? {
                         loop: !!item.loop,
-                        mp3: media(item.audio.path),
+                        mp3: item.audio.path,
                         // ogg: string
                         audio_credits: convertToHtml(item.audioCredits),
                       }
