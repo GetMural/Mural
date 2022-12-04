04edd8e58f5d54fd782c4efdcf65b7f54036c205
Flowdeeps <speedwolf@gmail.com>
2022-12-04 04:10:47 +0100

    Fixing the build pipeline still

--- frontend-webpack.config.js
+++ frontend-webpack.config.js
@@ -1,9 +1,10 @@
 const path = require('path')
-const ExtractTextPlugin = require('extract-text-webpack-plugin')
+// const ExtractTextPlugin = require('extract-text-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const { WebpackManifestPlugin } = require('webpack-manifest-plugin')
-const WebpackMd5Hash = require('webpack-md5-hash')
+// const WebpackMd5Hash = require('webpack-md5-hash')
 
-const extractSass = new ExtractTextPlugin({
+const miniCSSExtractPlugin = new MiniCssExtractPlugin({
   filename: '[name].[hash].css',
 })
 
@@ -22,40 +23,31 @@ module.exports = {
     rules: [
       {
         test: /\.scss|\.css$/,
-        use: ExtractTextPlugin.extract({
-          fallback: 'style-loader',
-          use: [
-            {
-              loader: 'css-loader',
-              options: {
-                // If you are having trouble with urls not resolving add this setting.
-                // See https://github.com/webpack-contrib/css-loader#url
-                url: false,
-                sourceMap: false,
-              },
-            },
-            {
-              loader: 'sass-loader',
-              options: {
-                sourceMap: false,
-              },
-            },
-          ],
-        }),
+        use: [MiniCssExtractPlugin.loader, "css-loader"]
       },
       {
         test: /\.svg$/,
-        loader: 'svg-loader',
+        use: {
+          options: {
+            loader: 'svg-loader'
+          }
+        }
       },
       {
         test: /\.(jpe?g|png|gif|svg)$/i,
-        // use: ['img-loader'],
-        loader: 'url-loader?limit=10000',
-      },
-    ],
+        use: {
+          options: {
+            loader: 'url-loader',
+            limit: 1000
+          }
+        }
+      }
+    ]
   },
-  plugins: [extractSass, new WebpackManifestPlugin(), new WebpackMd5Hash()],
+  // plugins: [extractSass, new WebpackManifestPlugin(), new WebpackMd5Hash()],
+  // plugins: [extractSass, new WebpackManifestPlugin()],
+  plugins: [new MiniCssExtractPlugin, new WebpackManifestPlugin()],
   externals: {
     'hls.js': 'Hls',
-  },
+  }
 }
